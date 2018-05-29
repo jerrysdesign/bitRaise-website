c1045f975bb08c70c57de89760b5d6b7e171f04a
JJ <jerry7722@hotmail.com>
2018-05-29 15:19:47 +0800

    remove vue sample

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,9 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    .site-line--top
+    .site-line--left
+    .site-line--right
+    .site-line--bottom
     router-view
 </template>
 <script>
@@ -18,4 +21,39 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+%site-line-stye {
+  position: fixed;
+  z-index: 999;
+  background: #e36969;
+}
+.site-line {
+  &--top {
+    @extend %site-line-stye;
+    width: 100%;
+    height: 12px;
+    top: 0;
+  }
+  &--right {
+    @extend %site-line-stye;
+    width: 12px;
+    height: 100%;
+    right: 0;
+    top:0;
+  }
+  &--bottom {
+    @extend %site-line-stye;
+    width: 100%;
+    height: 12px;
+    bottom:0;
+  }
+
+  &--left {
+    @extend %site-line-stye;
+    width: 12px;
+    height: 100%;
+    left: 0;
+    top:0;
+  }
+}
+
 </style>
---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  p {{ msg }}
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+      msg: 'Welcome to bitRaise'
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
