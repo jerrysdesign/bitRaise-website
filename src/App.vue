8cfdac4f596d7b51b73f94b6b6c88008e470b068
JJ <jerry7722@hotmail.com>
2018-05-29 14:52:08 +0800

    add pug

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
-<template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+<template lang="pug">
+  #app
+    img(src='./assets/logo.png')
+    router-view
 </template>
-
 <script>
 export default {
   name: 'App'
