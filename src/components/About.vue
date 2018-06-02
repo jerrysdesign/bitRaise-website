a0f5ef9e84808b189e1ad1f04f6ed2dc878640ea
JJ <jerry7722@hotmail.com>
2018-06-02 13:10:19 +0800

    update about

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,42 +5,46 @@
       .row
         .col-md-4
           h2.section-title.bottom_15
+            | Who we are?
             br
             span about us.
-          p
+          p 比特幣已經是一種大眾所熟知的數位貨幣，而比特幣背後的區塊鏈技術，勢必對人類帶來深厚的影響。超越貨幣的通證經濟及智能合約的區塊鏈應用，將繼超級電腦、個人電腦、網際網路和行動、社交網路之後的第五個破壞性創新科技。
+          p 我們認為區塊鏈本質是一本可以登記、註冊、轉移所有資產的公共帳本，具有在全球範圍內以去中心模式記錄資產的潛力，然而這些資產不僅是金融商品，也包含票選，軟體，健康數據，思想創意等，各式各樣的無形資產，與生態圈。
+
         .col-md-4
-          p
-          p
-              br
+          p 現今大眾對於區塊鏈技術仍停留在經濟層面，但其應用可以延伸到政治、公益、社會科學和科學領域，而且區塊鏈技術已備某些團體拿來解決現實世界的問題。
+          p 除了全球公共利益必須超越單一政府的情況之外，其他行業和社會階級，可以透過區塊鏈科技，擺脫受既得利益團體影響之下的不公平管制規約，而實現新的去中心化商業模式，尤其是有想法但沒錢實踐或量產的團隊，所以將願景透過通證，向群眾募資，並成承諾與實現。
+          p 對於成熟的企業而言，除了為其計畫擴展集資之外，更重要的是企業在數位貨幣和區塊鏈的時代佔據先機。引導越來越多的企業加入去中心化的業務和系統，向更加開放的功能，更為強大的基於區塊鏈的模式轉變。
         .col-md-4
-          p
+          p 我們期望可以與更多，相信區塊鏈技術的團隊合作，一同革命，透過ICO的方式，將區塊鏈技術加速普及與實現。
+          p 我們認為關鍵在於組建合適的團隊，因此我們擁有業界一流的區塊鏈開發，ICO諮詢、法律知識、市場行銷及業務管理方面的頂尖人才。
     // Service
     .services.padding_90.top_60
       .cont.row
+        .col-md-12.text-center.wow.fadeInUp
+          h2.section-title.bottom_60
+           | Full
+           br
+           span
+            | ICO services.
         .col-md-4.col-sm-6.service.text-center.bottom_60
           i.icon-desktop
           h3.title.top_15 ICO Concept building and Ideation
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
         .col-md-4.col-sm-6.service.text-center.bottom_60
           i.icon-tools
           h3.title.top_15 ICO White Paper
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
         .col-md-4.col-sm-6.service.text-center.bottom_60
           i.icon-paintbrush
           h3.title.top_15 Token Developement
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
         .col-md-4.col-sm-6.service.text-center
           i.icon-phone
           h3.title.top_15 ICO Website
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
         .col-md-4.col-sm-6.service.text-center
           i.icon-camera
           h3.title.top_15 Business Image Developement
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
         .col-md-4.col-sm-6.service.text-center
           i.icon-lightbulb
           h3.title.top_15 ICO Marketing
-          p Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum standard dummy text.
 </template>
 
 <script>
